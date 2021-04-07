91251566be974cb30f4768b1cf24d45638c3b658
cytern <2477185748@qq.com>
2021-04-07 23:12:39 +0800

    bug jenkins部署问题

--- jsoup-2030/src/test/java/test/dam/jsoup/updatereport/updatreport/service/impl/TestMissionAllTest.java
+++ jsoup-2030/src/test/java/test/dam/jsoup/updatereport/updatreport/service/impl/TestMissionAllTest.java
@@ -4,13 +4,6 @@ import org.junit.Test;
 import org.junit.Before; 
 import org.junit.After; 
 
-/** 
-* TestMissionAll Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>2ÔÂ 8, 2021</pre> 
-* @version 1.0 
-*/ 
 public class TestMissionAllTest { 
 
 @Before
