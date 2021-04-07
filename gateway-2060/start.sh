4e65ff99f69a3272badb0ce15ff981f9fc0d79f4
cytern <2477185748@qq.com>
2021-04-07 21:44:17 +0800

    需求 jenkins CICD自动化部署

---
+++ gateway-2060/start.sh
@@ -0,0 +1,20 @@
+#确定是否有程序启动
+jarName="gateway-dev.jar"
+jarPort="2060"
+pid12345=`lsof -i:$jarPort | sed -n "2, 1p" | awk '{print $2}'`
+if [ "$pid12345" = "" ]
+then
+ echo "端口未占用"
+else
+ echo "端口已占用，尝试关闭"
+ kill -9 "$pid12345"
+fi
+
+echo "mvn install gateway"
+mvn clean install -Pdev -Dmaven.test.skip=true
+echo "delete project gateway"
+# 删除 原来位置的jar包
+rm -rf /export/project/javaJar/$jarName
+cp -rf ./target/$jarName /export/project/javaJar
+#启动项目
+nohup java -jar /export/project/javaJar/$jarName &
