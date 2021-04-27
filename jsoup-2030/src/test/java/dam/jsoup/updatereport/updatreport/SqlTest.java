0587389221018f0cec5dda9165e0ccd36d37f5e0
cytern <2477185748@qq.com>
2021-04-27 17:02:46 +0800

    fix sql 语法错误

---
+++ jsoup-2030/src/test/java/dam/jsoup/updatereport/updatreport/SqlTest.java
@@ -0,0 +1,25 @@
+package dam.jsoup.updatereport.updatreport;
+
+import com.netflix.discovery.converters.Auto;
+import dam.jsoup.updatereport.updatreport.dao.GoodsMapper;
+import dam.jsoup.updatereport.updatreport.vo.GoodList;
+import dam.jsoup.updatereport.updatreport.vo.SearchGoodsVo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SqlTest {
+    @Autowired
+    private GoodsMapper goodsMapper;
+    @Test
+    void run () {
+        SearchGoodsVo searchGoodsVo = new SearchGoodsVo();
+        searchGoodsVo.setPageStart(0);
+        searchGoodsVo.setPageEnd(10);
+        List<GoodList> maList = goodsMapper.getMaList(searchGoodsVo);
+        System.out.println(maList);
+    }
+}
