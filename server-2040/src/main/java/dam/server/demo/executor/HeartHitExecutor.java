60bb034e97a973c431fdfc0494ddab0760637519
邓安民 <amdeng@che300.com>
2021-12-16 16:22:55 +0800

    feat: 修改配置文件 适应jdk11+

--- server-2040/src/main/java/dam/server/demo/executor/HeartHitExecutor.java
+++ server-2040/src/main/java/dam/server/demo/executor/HeartHitExecutor.java
@@ -24,39 +24,39 @@ public class HeartHitExecutor {
     public void heartHitSend ()  {
         ConfigBean configBean = ConfigBean.getInstance();
         try {
-            while (true) {
+            do {
                 log.info("******** 开始发送心跳包 *********");
                 Map<String, Object> map = null;
                 //发送心跳包
-                if (configBean.getJsoupSetting().getExecutor().getExecutorUrl()== null || configBean.getJsoupSetting().getExecutor().getExecutorUrl().equals("")) {
-                    Thread.sleep(10*1000);
+                if (configBean.getJsoupSetting().getExecutor().getExecutorUrl() == null || configBean.getJsoupSetting().getExecutor().getExecutorUrl().equals("")) {
+                    Thread.sleep(10 * 1000);
                     continue;
                 }
                 try {
                     map = HttpUtils.sendHeartHit(configBean.getHeartPag(), configBean.getCpuCoreId(), configBean.getJsoupSetting().getExecutor().getToken());
-                    log.info("********* 心跳返回结果  *********** =[{}]",map);
+                    log.info("********* 心跳返回结果  *********** =[{}]", map);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 if (map != null) {
-                    if ("success".equals(map.get("code"))){
+                    if ("success".equals(map.get("code"))) {
                         Integer status = (Integer) map.get("status");
                         for (int i = 0; i < status; i++) {
-                           if (taskExecutor.getActiveCount()>6) {
-                               break;
-                           }else {
-                               runJsoupService.run();
-                           }
+                            if (taskExecutor.getActiveCount() > 6) {
+                                break;
+                            } else {
+                                runJsoupService.run();
+                            }
                         }
                         configBean.setStatus(status);
                         configBean.setLive(1);
                     }
-                }else {
+                } else {
                     configBean.setLive(0);
                 }
                 //五分钟时间 休眠
-                Thread.sleep(2*60*1000);
-            }
+                Thread.sleep(2 * 60 * 1000);
+            } while (true);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
