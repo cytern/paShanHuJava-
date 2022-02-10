9f50b63397ae0e8a5c298d60aa70ef6b1359322b
cytern <cytern@qq.com>
2022-02-10 17:29:51 +0800

    fix:增加ai聊天功能

--- jsoup-2030/src/main/java/dam/jsoup/updatereport/updatreport/robot/command/impl/CommonCommand.java
+++ jsoup-2030/src/main/java/dam/jsoup/updatereport/updatreport/robot/command/impl/CommonCommand.java
@@ -6,11 +6,13 @@ import com.alibaba.fastjson.JSONObject;
 import dam.jsoup.updatereport.updatreport.robot.command.CommandReceiver;
 import dam.jsoup.updatereport.updatreport.robot.pojo.CommandData;
 import dam.jsoup.updatereport.updatreport.robot.pojo.MessageData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@Slf4j
 public class CommonCommand implements CommandReceiver {
     private static final String QYUrl = "http://api.qingyunke.com/api.php";
     private static final String key = "free";
@@ -24,9 +26,11 @@ public class CommonCommand implements CommandReceiver {
         paramMap.put("msg",commandData.getMessage());
         JSONObject body = JSONObject.parseObject(HttpRequest.get(QYUrl).form(paramMap).execute().body());
         String content = body.getString("content");
+        log.info("ai聊天接口 请求参数 {} 返回值{}",paramMap,body.toJSONString());
         if (content != null && !content.equals("")){
           //把换行修改掉
             content = content.replaceAll("\\{br}", "\n");
+            content = content.replaceAll("菲菲", "本姬");
         }
         commandData1.setMessage(content);
         return commandData1;
