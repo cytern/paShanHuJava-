c1b8e71dddc57bfdff85303f2dc1ee2000a7b559
cytern <2477185748@qq.com>
2022-04-12 20:40:50 +0800

    fix: 增加json版本的帝国时代分数生成 方便后续图片处理

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 git fetch
 git pull
-echo "更新分支完毕  准备重启euerka"
-start1=`sh ./euerka-2020/start.sh`
-echo "更新分支完毕  准备重启gateWay"
-start2=`sh ./gateway-2060/start.sh`
 echo "更新分支完毕  准备重启jsoup"
-start3=`sh ./jsoup-2030/start.sh`
+sh ./jsoup-2030/start.sh
